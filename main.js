f3d22a0775ea7b19a7504073654047246d9f87fc
Tanuj Aswani <tanuj.aswani@gmail.com>
2018-07-22 20:16:10 -0700

    Adding a variable to change the diplay per page value.
    I was showing 10 students per page, and I wanted that to be put into a variable. Now I can use that variable to switch the displayed students per page to any number I want (eg. 5 or 7 students per page).

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const studentsEmails = $('.email'); //variable to input all the students emails 
 let searchResults = []; //sets up an array to put all the search results into.
 let currentPage = 1; //sets up a current page variable to backtrack to the current page after a search.
 let searchMode = false; //boolean value to assess whether or not a search is being conducted.
+let numElements = 10; //setting up a variable to display a certain amount of students per page.
 
 //show page function to accurately hide students and display the ones that are between the range values.
 //also designed to only display current page after search mode has been set to off or False.
@@ -14,7 +15,7 @@ function showPage(pageNumber, sList) {
   }
   students.hide();
   for (let i = 0; i < sList.length; i++){
-    if ((10 * (pageNumber - 1)) <= i && i <= ((pageNumber * 10) - 1)) {
+    if ((numElements * (pageNumber - 1)) <= i && i <= ((pageNumber * numElements) - 1)) {
       sList[i].show();
     }
   };
@@ -24,11 +25,11 @@ function showPage(pageNumber, sList) {
 //takes in a different array for a parameter depending on search or just base student pagination.
 function appendPageLinks(sList) {
   let pageLinksSection = $('<div class="pagination"></div>').appendTo('.page');
-  let pageNums = Math.ceil(sList.length / 10); //variable to hold the amount of pagination links needed.
+  let pageNums = Math.ceil(sList.length / numElements); //variable to hold the amount of pagination links needed.
   for (let i = 0; i < pageNums; i++) {
     $('.pagination').append('<li><a href="#' + (i + 1) + '">' + (i + 1) + '</a></li>');
   };
-  $('a[href^="#1"]').addClass('active');
+  $('a[href="#1"]').addClass('active');
   $('a[href^="#"]').on('click', function(){
     $('a').removeClass('active');
     $(this).addClass('active');
